699ab4968a262c1035a5040d8de8fc8827bd7ea1
AZ Huang <aitjcize@gmail.com>
2013-02-06 21:36:04 +0800

    Remove uneeded code.

--- src/odbppviewwidget.cpp
+++ src/odbppviewwidget.cpp
@@ -21,36 +21,6 @@ ODBPPViewWidget::ODBPPViewWidget(QWidget* parent): QGraphicsView(parent)
   setWindowTitle(tr("test"));
 
   /*
-  Symbol* symbol = SymbolFactory::create("r50");
-  scene->addItem(symbol);
-  symbol->setPos(0, 0);
-
-  Symbol* symbol2 = SymbolFactory::create("rect100x50xr20x13");
-  scene->addItem(symbol2);
-  symbol2->setPos(100, 0);
-
-  Symbol* symbol3 = SymbolFactory::create("oval100x50");
-  scene->addItem(symbol3);
-  symbol3->setPos(0, 100);
-
-  Symbol* symbol4 = SymbolFactory::create("di100x50");
-  scene->addItem(symbol4);
-  symbol4->setPos(0, -100);
-
-  Symbol* symbol5 = SymbolFactory::create("oct60x60x20");
-  scene->addItem(symbol5);
-  symbol5->setPos(-100, 0);
-
-  Symbol* symbol6 = SymbolFactory::create("donut_r60x30");
-  scene->addItem(symbol6);
-  symbol6->setPos(-100, -100);
-
-  Symbol* symbol7 = SymbolFactory::create("s40");
-  scene->addItem(symbol7);
-  symbol7->setPos(100, -100);
-  */
-
-  /*
   Symbol* user = new UserSymbol("");
   scene->addItem(user);
   user->setPos(0, 0);
