93b059a21f334d30bef9a55db3f83f41657ca528
B.R. Wu <killuaken@gmail.com>
2013-02-14 17:20:31 +0800

    Fix small issue about gap implementation

--- src/codegen/milk_script.sh
+++ src/codegen/milk_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-SYMBOL_NAME=hexagon_l
+SYMBOL_NAME=roundthermal_s
 ./codegen_milk.py -t symbol.h.tmpl -C ../symbol ${SYMBOL_NAME}.xml 
 ./codegen_milk.py -t symbol.cpp.tmpl -C ../symbol ${SYMBOL_NAME}.xml
