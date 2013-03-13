e15b60a86eccf59e4052b178f8ce42f94c27aef6
AZ Huang <aitjcize@gmail.com>
2013-03-14 00:41:52 +0800

    Move initialization back to main because unkonw cause of segfault on Windows.

--- src/context.cpp
+++ src/context.cpp
@@ -1,15 +1,7 @@
 #include "context.h"
 
-#include "code39.h"
-#include "context.h"
-#include "settings.h"
-
 Context ctx;
 
 Context::Context()
 {
-  Code39::initPatterns();
-  Settings::load("config.ini");
-
-  ctx.bg_color = QColor(SETTINGS->get("Color", "BG").toString());
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <QtGui>
 
+#include "code39.h"
+#include "context.h"
 #include "jobmanagerdialog.h"
+#include "settings.h"
 
 int main(int argc, char *argv[])
 {
   QApplication app(argc, argv);
   QApplication::setGraphicsSystem("raster");
   QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
+
+  Code39::initPatterns();
+  Settings::load("config.ini");
+  ctx.bg_color = QColor(SETTINGS->get("Color", "BG").toString());
 
   JobManagerDialog dialog;
   dialog.show();
