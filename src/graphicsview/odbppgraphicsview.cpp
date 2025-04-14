03a874c4cfca24113ed9950978378622f36bb19f
Jörg Wunsch <j_git@uriah.heep.sax.de>
2025-04-14 22:28:02 +0200

    ODBPPGraphicsView::wheelEvent(): use angleDelta
    pixelDelta is not necessarily available on all implementations.
    Use angleDelta instead.

--- src/graphicsview/odbppgraphicsview.cpp
+++ src/graphicsview/odbppgraphicsview.cpp
@@ -210,7 +210,7 @@ void ODBPPGraphicsView::updateLayerViewport(void)
 void ODBPPGraphicsView::wheelEvent(QWheelEvent *event)
 {
   setTransformationAnchor(AnchorUnderMouse);
-  scaleView(pow((double)2, -event->pixelDelta().y() / 240.0));
+  scaleView(pow((double)2, -event->angleDelta().y() / 240.0));
 }
 
 void ODBPPGraphicsView::keyPressEvent(QKeyEvent* event)
